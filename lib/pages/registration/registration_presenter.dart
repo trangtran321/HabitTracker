18593f0d7e7ba2665e33167377dedf1e3bccb7d0
Trang Tran <111462670+trangtran321@users.noreply.github.com>
2023-03-27 15:43:37 -0600

    registration page

---
+++ lib/pages/registration/registration_presenter.dart
@@ -0,0 +1,25 @@
+
+import 'package:habit_tracker/database/rest_data.dart';
+import 'package:habit_tracker/models/user.dart';
+
+abstract class RegisterPageContract {
+  void onRegisterSuccess(User user);
+  void onRegisterError(String error);
+}
+
+class RegisterPagePresenter {
+  RegisterPageContract _view;
+  RestData api = new RestData();
+  RegisterPagePresenter(this._view);
+
+  doRegister(String username, String password) {
+    //print("HI");
+    api
+        .login(username, password)
+        .then((user) => _view.onRegisterSuccess(user))
+        .catchError((onError) {
+          //print("Trying to Catch"+onError.toString());
+          return _view.onRegisterError(onError.toString());
+    });
+  }
+}
